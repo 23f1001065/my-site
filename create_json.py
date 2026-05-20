7d832daefe23371336eb5dbe76618ba75cb351b4
Akash <23f1001065@ds.study.iitm.ac.in>
2026-05-20 08:36:55 +0530

    Corrected code
    code inside f.write()

--- create_json.py
+++ create_json.py
@@ -3,7 +3,7 @@ from datetime import datetime, UTC
 def create_file():
   now = datetime.now(UTC)
   with open(f'demo-file-{now.strftime("%Y-%m-%d")}.json') as f:
-    f.write(json.dumps({"timestamp": now.isoformat(), "complete_time": now} + "\n")
+    f.write(json.dumps({"timestamp": now.isoformat(), "complete_time": now}))
 
 if __name__=="__main__":
   create_file()
